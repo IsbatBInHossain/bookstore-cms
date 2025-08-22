79ce748b6351ae437db6eb51a2f9e15081795679
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 01:24:06 +0600

    feat(account-service): add registerUser service

--- apps/account-service/src/modules/auth/auth.service.ts
+++ apps/account-service/src/modules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import type { registerSchemaType } from './auth.validation.js';
 import { prisma } from '../../core/prisma-client.js';
 import { logger } from '../../core/server.js';
+import { hashPassword } from '../../shared/utils/password.js';
 
 export const registerUser = async (userData: registerSchemaType) => {
   const existingUser = await prisma.user.findUnique({
@@ -14,7 +15,35 @@ export const registerUser = async (userData: registerSchemaType) => {
     throw new Error('User with given email already exits.');
   }
 
+  const hash = await hashPassword(userData.password);
+
   try {
-    // prisma.$transaction(tx => {});
-  } catch (error) {}
+    prisma.$transaction(async tx => {
+      const { email, firstName, lastName, phone } = userData;
+      const user = await tx.user.create({
+        data: {
+          email,
+          passwordHash: hash,
+          roleId: 'CUSTOMER',
+        },
+      });
+
+      const profile = tx.userProfile.create({
+        data: {
+          firstName,
+          lastName: lastName ?? null,
+          phone: phone ?? null,
+          userId: user.id,
+        },
+      });
+      const { passwordHash, ...safeUser } = user;
+      return {
+        ...safeUser,
+        profile: { ...profile },
+      };
+    });
+  } catch (err) {
+    logger.warn('Failed to register user.');
+    throw new Error('Failed to register user.');
+  }
 };
