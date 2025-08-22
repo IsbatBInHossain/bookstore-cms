2fa13e685d912fe0468346746cf3d337a870eec0
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 01:43:30 +0600

    feat(account-service): create shared custom error type

---
+++ apps/account-service/src/core/api-error.ts
@@ -0,0 +1,19 @@
+export class ApiError extends Error {
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
+
+  constructor(
+    statusCode: number,
+    message: string,
+    isOperational: boolean = true
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ApiError);
+    }
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
+}
