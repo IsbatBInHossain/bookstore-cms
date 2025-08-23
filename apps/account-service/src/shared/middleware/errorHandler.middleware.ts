d0e3cb831ee091ff9c569009c9b7586f3dd12b23
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 14:32:08 +0600

    refactor(accout-service): move global error handler from server to middleware

---
+++ apps/account-service/src/shared/middleware/errorHandler.middleware.ts
@@ -0,0 +1,23 @@
+import { ApiError } from '../../core/api-error.js';
+import type { Request, Response, NextFunction } from 'express';
+import { sendErrorResponse } from '../handlers/responseHandlers.js';
+import { logger } from '../../core/server.js';
+
+export const errorHandler = (
+  err: unknown | ApiError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!(err instanceof ApiError)) {
+    const err = new ApiError(500, 'Something went wrong', false);
+    return sendErrorResponse(res, err.statusCode, err.message);
+  } else {
+    if (err?.isOperational) {
+      logger.warn(err);
+    } else {
+      logger.error(err);
+    }
+    return sendErrorResponse(res, err.statusCode, err.message);
+  }
+};
