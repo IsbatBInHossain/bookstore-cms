83de5a9979e3cd18bc3ec46aa1c9a357bdccb9c3
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 21:47:56 +0600

    feat(account-service): create auth middleware to authenticate using jwt

--- apps/account-service/src/core/server.ts
+++ apps/account-service/src/core/server.ts
@@ -1,16 +1,14 @@
-import type { Express, NextFunction, Request, Response } from 'express';
+import type { Express, Request, Response } from 'express';
 import express from 'express';
 import helmet from 'helmet';
 
 // Routers
 import authRouter from '../modules/auth/auth.routes.js';
-import {
-  sendErrorResponse,
-  sendSuccessResponse,
-} from '../shared/handlers/response.handler.js';
+import { sendSuccessResponse } from '../shared/handlers/response.handler.js';
 import { ApiError } from './api-error.js';
 import { errorHandler } from '../shared/middleware/errorHandler.middleware.js';
-import { logger } from '../shared/utils/logger.js';
+import passport from 'passport';
+import jwtStrategy from './passport.js';
 
 // Constants
 const API_BASE = '/api/v1';
@@ -22,6 +20,10 @@ export const createServer = (): Express => {
   app.use(helmet());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  // --- Initialize Passport ---
+  app.use(passport.initialize());
+  passport.use(jwtStrategy);
 
   // Health check endpoint
   app.get(`${API_BASE}/health`, (req: Request, res: Response) => {
