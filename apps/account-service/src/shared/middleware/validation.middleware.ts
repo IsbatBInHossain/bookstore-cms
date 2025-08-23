4638551eee692a74121712858242d69d75a26a46
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 13:12:14 +0600

    feat(accout-service): create shared middleware to validate zod schema

---
+++ apps/account-service/src/shared/middleware/validation.middleware.ts
@@ -0,0 +1,40 @@
+import { ZodSchema, ZodError } from 'zod';
+import type { Response, Request, NextFunction } from 'express';
+import { logger } from '../../core/server.js';
+import { sendErrorResponse } from '../handlers/responseHandlers.js';
+
+/**
+ * Middleware to validate incoming request data using Zod schema
+ * @param {ZodSchema} schema - Zod schema to validate against
+ * @returns Express middleware function
+ */
+export const validateSchema =
+  (schema: unknown) => (req: Request, res: Response, next: NextFunction) => {
+    if (!(schema instanceof ZodSchema)) {
+      logger.error('Invalida or missing zod schema');
+      return sendErrorResponse(res, 500, 'Internal server error');
+    }
+
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      next();
+    } catch (err) {
+      let formattedErrors = null;
+      let errorMessage = 'Validation failed';
+      if (err instanceof ZodError && err.errors && Array.isArray(err.errors)) {
+        formattedErrors = err.errors.map(e => ({
+          field: e.path.join('.'),
+          message: e.message,
+        }));
+      } else {
+        errorMessage =
+          'An unexpected error occurred during request validation.';
+      }
+      logger.error(errorMessage);
+      sendErrorResponse(res, 400, errorMessage, formattedErrors);
+    }
+  };
