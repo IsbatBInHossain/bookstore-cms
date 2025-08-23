f7ae1feca08c400df923f3c2336f00d8fa79b3fc
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-23 16:03:15 +0600

    refactor(account-service): remove unnecessary try catch block from the auth controller

--- apps/account-service/src/modules/auth/auth.controller.ts
+++ apps/account-service/src/modules/auth/auth.controller.ts
@@ -1,25 +1,14 @@
 import type { NextFunction, Request, Response } from 'express';
 import { authService } from './auth.service.js';
-import { ApiError } from '../../core/api-error.js';
-import {
-  sendErrorResponse,
-  sendSuccessResponse,
-} from '../../shared/handlers/response.handler.js';
+import { sendSuccessResponse } from '../../shared/handlers/response.handler.js';
 
 const registerUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  try {
-    const user = await authService.registerUser(req.body);
-    return sendSuccessResponse(res, 201, 'Succesfully created user', user);
-  } catch (error) {
-    if (error instanceof ApiError) {
-      return sendErrorResponse(res, error.statusCode, error.message, error);
-    }
-    next(error);
-  }
+  const user = await authService.registerUser(req.body);
+  return sendSuccessResponse(res, 201, 'Succesfully created user', user);
 };
 
 export const authController = {
