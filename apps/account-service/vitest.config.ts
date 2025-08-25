a0747dcff37f7b20ca8bf0a97557532e698c6314
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-25 12:13:22 +0600

    chore: add vitest, testcontiner and supertest packages and confiure vitest

---
+++ apps/account-service/vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    // For integration tests that share a single database container, it's safer to run them serially.
+    fileParallelism: false,
+
+    // Make the test APIs (`describe`, `it`, `expect`) available globally
+    globals: true,
+
+    // A long timeout is needed because starting a Docker container
+    // for the first time can take a while (image pulling).
+    // 60 seconds should be more than enough.
+    testTimeout: 60000,
+    hookTimeout: 60000,
+
+    // Location of setup file
+    setupFiles: ['./src/test/setup.ts'],
+
+    // Test file path
+    include: ['src/**/*.test.ts'],
+  },
+});
