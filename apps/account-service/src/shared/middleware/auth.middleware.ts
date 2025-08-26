24dcc17564e72f5329ccc14fcb770debdf8e2e5d
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-26 20:07:08 +0600

    refactor(account-service): passport authentication middleware to send custom error response

--- apps/account-service/src/shared/middleware/auth.middleware.ts
+++ apps/account-service/src/shared/middleware/auth.middleware.ts
@@ -1,5 +1,7 @@
 import passport from 'passport';
 import type { Request, Response, NextFunction } from 'express';
+import { sendErrorResponse } from '../handlers/response.handler.js';
+
 /**
 Middleware to protect routes by requiring a valid JWT access token.
 */
@@ -10,5 +12,22 @@ export const authenticate = (
 ) => {
   // Use the 'jwt' strategy, and since we are not using cookies, session is false.
   // Passport will automatically handle the 401 error response if authentication fails.
-  passport.authenticate('jwt', { session: false })(req, res, next);
+  passport.authenticate(
+    'jwt',
+    { session: false },
+    (
+      err: unknown,
+      user: Express.User | false,
+      info: { message?: string } | undefined
+    ) => {
+      if (err) return next(err);
+
+      if (!user) {
+        return sendErrorResponse(res, 401, 'Unauthorized', info);
+      }
+
+      req.user = user;
+      next();
+    }
+  )(req, res, next);
 };
