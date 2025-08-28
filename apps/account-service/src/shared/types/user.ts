6e8ac7e0760ab7fc12e7c2a311e68e105fbabaa4
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-08-28 20:50:29 +0600

    chore(account-service): update seedDatabase funciton to seed role permissions table

--- apps/account-service/src/shared/types/user.ts
+++ apps/account-service/src/shared/types/user.ts
@@ -1,8 +1,11 @@
+import type { RolePermission } from '../../generated/prisma/index.js';
+
 export interface UserResponsePayload {
   id: string;
   email: string;
   role: {
     name: string;
+    permissions?: RolePermission[];
   };
   profile: {
     firstName?: string | null;
